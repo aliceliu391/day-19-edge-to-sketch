3cd9830856cd3dde0c631a345286b8e587657daa
aliceliu391 <112590839+aliceliu391@users.noreply.github.com>
2024-08-17 13:27:11 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.back(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def clear():
+    tim.clear()
+    tim.up()
+    tim.home()
+    tim.down()
+
+
+screen.listen()
+screen.onkey(fun=move_forward, key="w")
+screen.onkey(fun=move_backward, key="s")
+screen.onkey(fun=turn_right, key="d")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=clear, key="c")
+
+screen.exitonclick()
+
